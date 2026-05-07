32bb9c2b261501d917ca12dbcd473ec199b78eb5
Han Nguyen <hdnguy2@ilstu.edu>
2026-05-07 14:01:03 -0500

    Update main.sh
    Commented out code.

--- main.sh
+++ main.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+# This is the "main.sh" script. Its purpose is to orchestrate the modules of the Linux Live Response forensic collection tool.
 echo "Starting Linux Live Response Collection..."
 
-# Define output directory:=
+# Output directory definition: this is a unique output directory using a Year, Month, Day, Hour, Minute, Second format, 
+# to prevent overwrites of previous forensic runs, and to validate evidence with a precise, chronological timeline of 
+#actions and sequence of events.
 OUTPUT_DIR="output_$(date +%Y%m%d_%H%M%S)"
 mkdir -p "$OUTPUT_DIR"
 
-# Timestamp
+# Timestamp capturing the exact start time of the investigation, to aid the forensic timeline.
 date > "$OUTPUT_DIR/timestamp.txt"
 
+# Module execution phase:
+# Each module is based off a standalone script, located in /modules, for the purpose of modularity and clean code.
+# Modules include system_info.sh, users_auth.sh, processes.sh, network.sh, logs.sh,  and persistence.sh
+# All module results are saved into the unique OUTPUT_DIR created above.
 echo "Running System Info Module..."
 bash modules/system_info.sh "$OUTPUT_DIR"
 
@@ -29,7 +36,8 @@ bash modules/persistence.sh "$OUTPUT_DIR"
 
 echo "Collection Complete. Output: $OUTPUT_DIR"
 
-# Outputs get aggregated and bundled into one file, AI_BUNDLE
+# Data aggregation phase
+# Outputs get aggregated and bundled into one single file, AI_BUNDLE, for AI processing.
 AI_BUNDLE="$OUTPUT_DIR/ai_bundle.txt"
 
 echo "Creating AI input bundle..."
@@ -54,7 +62,8 @@ cat "$OUTPUT_DIR/persistence/"* >> "$AI_BUNDLE" 2>/dev/null
 
 echo "AI bundle created at: $AI_BUNDLE"
 
-# AI Integration: Feed AI_BUNDLE into Gemini for a digital forensics report
+# AI integration and analysis phase:
+# Feeds the aggregated forensic data AI_BUNDLE into the Gemini API for a digital forensics report.
 echo "Running AI analysis..."
 
 python3 modules/ai.py "$AI_BUNDLE" > "$OUTPUT_DIR/ai_analysis.txt"
